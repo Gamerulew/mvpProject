4278d9c26ac46abfb07b0c9cb78bbc4be9fbb472
PedroL <pedrolucassantosferreira426@gmail.com>
2021-02-14 20:56:33 -0300

    config prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  SERVER_API_URL: `https://api.mundodocodigo.com.br/wokProd/`,
+  SERVER_API_IMAGE_URL: `'https://api.mundodocodigo.com.br/storage/'`
 };
